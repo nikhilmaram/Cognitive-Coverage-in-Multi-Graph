138d8e431997f0009f70d1d88325389fa8caef43
nikhilmaram <nike.maram@gmail.com>
2017-12-13 00:23:22 -0800

    Implemented the functionality where number of features is greater than number of selected nodes

--- DiverseCoverage.py
+++ DiverseCoverage.py
@@ -4,6 +4,8 @@ import copy
 import time
 import creatingGraph as cg
 
+featuresPresent = ["feature1","feature2","feature3"]
+
 
 class Node:
     def __init__(self,name,feature1,feature2,feature3):
@@ -17,6 +19,15 @@ class Node:
 
     def __str__(self):
         return self.name
+    def __repr__(self):
+        return self.name
+
+class coverageClass:
+    def __init__(self,nodeList,featureCountList):
+        self.nodeList = nodeList
+        self.featureCountList = featureCountList
+        #print(self.nodeList)
+        #print(len(self.featureCountList))
 
 class GraphClass:
     def __init__(self,name):
@@ -76,11 +87,9 @@ class GraphClass:
                 if(getattr(node,feature) == True):
                     self.createEdge(virtualNode,node)
 
-
     def makeNodesVisited(self,node):
         for neighbors in self.G.neighbors(node):
             neighbors._visited = True
-
 
     def markNodesUnvisited(self):
         for node in self.G.nodes:
@@ -138,12 +147,94 @@ class GraphClass:
                 node2 = nodeList[j]
                 self.createEdge(node1, node2)
 
+    def choseNodeWithAllFeatures(self,fullCoverageSet,featureList):
+        maxFeatureCount = 0
+        maxCoverageNode = fullCoverageSet[0]
+        containsAllFeatures = True
+        for coverage in fullCoverageSet:
+            featureCountList = coverage.featureCountList
+            featureCount = 0
+            containsAllFeatures = True
+            for value in featureCountList.values():
+                if value == 0:
+                    containsAllFeatures = False
+                featureCount += value
+
+            if (containsAllFeatures and (featureCount > maxFeatureCount)):
+                maxFeatureCount = featureCount
+                maxCoverageNode = coverage
+
+        return maxCoverageNode.nodeList,maxFeatureCount
+
+
+    def choseLessBestNodes(self,virtualNodeList,countFeatures = False):
+        nodesChosen = []
+        totalCount = 0
+        ## Need to chose nodes which has the same feature as virtual node and has high degree.
+        ## We are considering all the neighbors of the vertex chosen. Not only the nodes with same feature as virtual node.
+        for vNodeList in virtualNodeList:
+            maxNeighborLength = 0
+            maxNeightbor = vNodeList[0]
+            features = []
+            ## Find the features that correspond to virtual Nodes present in virtual node list
+            for vNode in vNodeList:
+                for feature in featuresPresent:
+                    if(hasattr(vNode,feature) and getattr(vNode,feature) == True):
+                        features.append(feature)
+            coverageObjectList = []
+            ## need to chose a best vertex from the neighbors of vnodes
+            for vNode in vNodeList:
+                for node in self.G.neighbors(vNode):
+                    if node._visited == False:
+                        nLength = self.neighborLength(self.G.neighbors(node))+1
+                        if not countFeatures:
+                            ## This choses the node with maximum neighbors
+                            if (nLength > maxNeighborLength):
+                                maxNeighborLength = nLength
+                                maxNeightbor = node
+                        else:
+                            featureCount = {}
+                            ## Check if the feature present in the node
+                            for feature in features:
+                                featureCount[feature] = 0
+                                if hasattr(node, feature) and (getattr(node, feature) == True):
+                                    featureCount[feature] += 1
+                            ## We can also use the maximising function like in brute force
+                            ## Checking the neighbors of the nodes
+                            for neighbor in self.G.neighbors(node):
+                                for feature in features:
+                                    if hasattr(neighbor, feature) and (getattr(neighbor, feature) == True):
+                                        featureCount[feature] += 1
+                            
+                            coverageObject = coverageClass([node], featureCount)
+                            coverageObjectList.append(coverageObject)
+
+            ## Chose the best node from all the nodes if count features chosen
+            if countFeatures:
+                if len(coverageObjectList):
+                    maxNeightborList,maxNeighborLength = self.choseNodeWithAllFeatures(coverageObjectList,features)
+                    ## Because the above function returns the list
+                    maxNeightbor = maxNeightborList[0]
+                else:
+                    maxNeighborLength = 0
+                    maxNeightbor = vNodeList[0]
+
+            maxNeightbor._visited = True
+            nodesChosen.append(maxNeightbor)
+            self.makeNodesVisited(maxNeightbor)
+            totalCount+=maxNeighborLength
+
+        return nodesChosen,totalCount
+
 
 
 def permute(vnodeList,l,r,combinationList):
 
     if (l==r):
-        combinationList.append(copy.copy(vnodeList))
+        nodeComb = []
+        for node in vnodeList:
+            nodeComb.append([node])
+        combinationList.append(nodeComb)
     else:
         for i in range(l,r+1):
             vnodeList[l],vnodeList[i] = vnodeList[i],vnodeList[l]
@@ -161,6 +252,17 @@ def createBasicGraph(Graph):
     Node8 = Graph.createNode(8, False, False, False)
     Node9 = Graph.createNode(9, True, True, True)
     Node10 = Graph.createNode(10, False, True, True)
+
+    cg.name2Node['1'] = Node1
+    cg.name2Node['2'] = Node2
+    cg.name2Node['3'] = Node3
+    cg.name2Node['4'] = Node4
+    cg.name2Node['5'] = Node5
+    cg.name2Node['6'] = Node6
+    cg.name2Node['7'] = Node7
+    cg.name2Node['8'] = Node8
+    cg.name2Node['9'] = Node9
+    cg.name2Node['10'] = Node10
 
     ## Creating edges between the nodes
     Graph.createEdge(Node1, Node2)
@@ -197,16 +299,17 @@ def createVirtualGraph(vGraph):
     vGraph.createBipartiteGraph(v2, "feature2")
     vGraph.createBipartiteGraph(v3, "feature3")
 
-    a = [v1, v2, v3]
-    combinationList = []
-    ## Permute all different combinations of virtual nodes to be chosen
-    permute(a, 0, len(a) - 1, combinationList)
-    maxCoverage = 0
+    return [v1,v2,v3]
 
+
+def bestNodeSet(vGraph,combinationList):
+    maxCoverageNodeSet = []
+    maxCoverage = 0
     for comb in combinationList:
         vNodePresent = False
         ## For each combination chose the best nodes
-        nodesChosen, coverage = vGraph.choseBestNodes(comb)
+        # print(comb)
+        nodesChosen, coverage = vGraph.choseLessBestNodes(comb, True)
         for node in nodesChosen:
             if (node.virtual == True):
                 vNodePresent = True
@@ -220,8 +323,29 @@ def createVirtualGraph(vGraph):
 
     ## if all the combinations contain a virtual node
 
-
     return maxCoverageNodeSet
+
+def createVirtualGraphWithLessNodesChosen(vGraph):
+    ## Creating virtual nodes
+    v1 = vGraph.createVirutalNode('v1', True, False, False)
+    v2 = vGraph.createVirutalNode('v2', False, True, False)
+    v3 = vGraph.createVirutalNode('v3', False, False, True)
+
+    ## Adding virtual nodes to the graph
+    vGraph.addNode(v1)
+    vGraph.addNode(v2)
+    vGraph.addNode(v3)
+    ## Build the bipartite graph from virtual nodes
+
+    vGraph.createBipartiteGraph(v1, "feature1")
+    vGraph.createBipartiteGraph(v2, "feature2")
+    vGraph.createBipartiteGraph(v3, "feature3")
+
+    a = [v1, v2, v3]
+    combinationList = [[[v1,v2],[v3]],[[v1,v3],[v2]],[[v2,v3],[v1]]]
+    ## Permute all different combinations of virtual nodes to be chosen
+    ##permute(a, 0, len(a) - 1, combinationList)
+
 
 if __name__ == "__main__":
 
@@ -231,12 +355,23 @@ if __name__ == "__main__":
     ##createBasicGraph(Graph)
     cg.creatingGraph(Graph,0,5100)
 
+    lessNodes = False
+
     ##Graph.drawGraphColors()
-    for i in range(4):
+    ## When features same as number of people chosen
+    for i in range(20):
         ## Building the virutal Graph
         vGraph = GraphClass("virtual Graph")
         vGraph.G = Graph.G.copy()
-        maxCoverageSet = createVirtualGraph(vGraph)
+        vNodeList = createVirtualGraph(vGraph)
+        if not lessNodes:
+            combinationList = []
+            permute(vNodeList, 0, 2, combinationList)
+        else:
+            combinationList = [[[vNodeList[0], vNodeList[1]], [vNodeList[2]]],
+                               [[vNodeList[2], vNodeList[1]], [vNodeList[0]]],
+                               [[vNodeList[0], vNodeList[2]], [vNodeList[1]]]]
+        maxCoverageSet = bestNodeSet(vGraph,combinationList)
         print("---------------------------------------")
         for node in maxCoverageSet:
             print(node)
